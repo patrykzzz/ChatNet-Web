e32d685e5481ff8fdf95481f4bb2054038ba438f
Patryk Miłek <patrykmilek96@gmail.com>
2019-07-08 12:23:27 +0200

    Add angular material module

---
+++ src/app/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatIconModule } from "@angular/material";
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonModule,
+    MatIconModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule
+  ]
+})
+export class MaterialModule { }
