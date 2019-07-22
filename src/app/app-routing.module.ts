ea470be9d848585d5297ee61cb277e348752efb9
Patryk Miłek <patrykmilek96@gmail.com>
2019-07-22 16:31:11 +0200

    Style menu and add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
+import { ChatRoomListComponent } from './components/chat-room-list/chat-room-list.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'chatrooms', component: ChatRoomListComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { LoginComponent } from './components/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ChatRoomListComponent } from './components/chat-room-list/chat-room-list.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ChatRoomListComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
